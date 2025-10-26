50e7bca0351c4a85d6db1dcdad2606f0d1035faa
kobayashi5829 <jimonjitou5884@gmail.com>
2025-10-26 14:19:22 +0900

    push

--- ch05.py
+++ ch05.py
@@ -1,5 +1,7 @@
 import torch
 import tiktoken
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from ch02 import create_dataloader_v1
 from ch04 import GPTModel
 from ch04 import generate_text_simple
@@ -100,6 +102,23 @@ def generate_and_print_sample(model, tokenizer, device, start_context):
     decoded_text = token_ids_to_text(token_ids, tokenizer)
     print(decoded_text.replace("\n", " "))
     model.train()
+
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(
+        epochs_seen, val_losses, linestyle="-.", label="Validation loss"
+    )
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    plt.show()
 
 def main():
     GPT_CONFIG_124M = {
